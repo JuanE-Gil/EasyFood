37a9093d06228b1262e3747ebdf3f016a92ddd21
Juan E. Gil <JuanE.Gil2002@Gmail.com>
2023-12-08 00:40:42 -0500

    Initial commit

---
+++ app/src/main/java/com/juangil/easyfood/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.juangil.easyfood.utils;
+
+public class Utils {
+
+    private static final int DURACION_SPLASH_SCREEN = 3000;
+
+    public int getDuracionSplashScreen() {
+        return DURACION_SPLASH_SCREEN;
+    }
+
+}
---
+++ app/src/main/java/com/juangil/easyfood/views/splashscreenview/SplashScreenActivity.java
@@ -0,0 +1,46 @@
+package com.juangil.easyfood.views.splashscreenview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.juangil.easyfood.MainActivity;
+import com.juangil.easyfood.R;
+import com.juangil.easyfood.databinding.ActivitySplashScreenBinding;
+import com.juangil.easyfood.utils.Utils;
+import com.juangil.easyfood.views.homescreenview.HomeScreenActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Glide
+                .with(this)
+                .load(R.drawable.git_logo)
+                .centerInside()
+                .into(binding.ivSplashScreen);
+
+        cambiarPantalla();
+    }
+
+    private void cambiarPantalla() {
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, HomeScreenActivity.class);
+            startActivity(intent);
+            finish();
+        }, new Utils().getDuracionSplashScreen());
+    }
+}
